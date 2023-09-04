3e9ea470a2a4b58a7d2e0e669ba14455dcebf19d
hpcatley <hpcatley@gmail.com>
2023-09-04 12:05:08 +0930

    main commit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,32 @@
 #include "BubbleSort.h"
 #include "QuickSort.h"
 
-int main() {
+#include <string>
+#include <iostream>
+#include <vector>
 
+
+int main() {
+    std::vector<int> List;
+    // Need to take in growing vector, thus size cannot be defined.
+    // Take a string as input, and convert it to a vector of characters, back into the List vector.
+    std::string stringList;
+    std::cout << "Please enter a vector, with the elements separated by a space. ";
+    std::cout << "Click enter when complete." << std::endl;
+    // Take the whole line as input
+    std::getline(std::cin, stringList);
+    std::cout << stringList;
+
+    // Convert to vector
+    
+
+    // Create a sorting class to sort the list
+    Sort sorting;
+    std::vector<int> sortedList = sorting.sort(List);
+
+    // Output the list
+    for (int i=0; i<sizeof(sortedList); i++)
+        std::cout << sortedList[i];
     return 0;
+
 }
